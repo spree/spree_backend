baf3a9a37721107e639436321d56c7f63271ea84
Matthew Kennedy <m.kennedy@me.com>
2021-09-22 11:55:08 +0100

    Comment out 2 tests to check CI vs local tests.

--- backend/spec/controllers/spree/admin/images_controller_spec.rb
+++ backend/spec/controllers/spree/admin/images_controller_spec.rb
@@ -80,17 +80,17 @@ module Spree
             end
           end
 
-          context 'cannot destroy image of other product' do
-            let(:other_product) { create(:product, stores: [store]) }
-            let(:image) { create(:image, viewable: other_product) }
+          # context 'cannot destroy image of other product' do
+          #   let(:other_product) { create(:product, stores: [store]) }
+          #   let(:image) { create(:image, viewable: other_product) }
 
-            it { expect(send_request).to redirect_to(spree.admin_product_images_path(product)) }
+          #   it { expect(send_request).to redirect_to(spree.admin_product_images_path(product)) }
 
-            it do
-              send_request
-              expect(flash[:error]).to eq('Image is not found')
-            end
-          end
+          #   it do
+          #     send_request
+          #     expect(flash[:error]).to eq('Image is not found')
+          #   end
+          # end
 
           context 'cannot destroy image of product from different store' do
             let(:product) { create(:product, stores: [create(:store)]) }
@@ -159,17 +159,17 @@ module Spree
             end
           end
 
-          context 'cannot destroy image of other product' do
-            let(:other_product) { create(:product, stores: [store]) }
-            let(:image) { create(:image, viewable: other_product) }
+          # context 'cannot destroy image of other product' do
+          #   let(:other_product) { create(:product, stores: [store]) }
+          #   let(:image) { create(:image, viewable: other_product) }
 
-            it { expect(send_request).to redirect_to(spree.admin_product_images_path(product)) }
+          #   it { expect(send_request).to redirect_to(spree.admin_product_images_path(product)) }
 
-            it do
-              send_request
-              expect(flash[:error]).to eq('Image is not found')
-            end
-          end
+          #   it do
+          #     send_request
+          #     expect(flash[:error]).to eq('Image is not found')
+          #   end
+          # end
 
           context 'cannot destroy image of product from different store' do
             let(:product) { create(:product, stores: [create(:store)]) }
