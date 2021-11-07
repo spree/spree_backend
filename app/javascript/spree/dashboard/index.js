61ee3e784c2843af01aed0190804386c3fa78bbe
Damian Legawiec <damian@sparksolutions.co>
2021-11-07 09:32:49 +0100

    Added ESbuild via JS Bundling gem (#18)
    * Setup Hotwire and NPM package
    
    * fix install generator
    
    * Setup ESlint

---
+++ app/javascript/spree/dashboard/index.js
@@ -0,0 +1,13 @@
+import "./controllers"
+// import * as Turbo from "@hotwired/turbo"
+
+class Dashboard {
+  constructor() {
+    console.log("Spree Dashboard initialized")
+  }
+}
+
+export {
+  Dashboard
+  // Turbo
+}
