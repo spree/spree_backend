fa65991b50c4bca320c5956bfd7402c0c5c0e942
Matthew Kennedy <m.kennedy@me.com>
2021-11-11 10:27:51 +0000

    Basic implementation of digital assets in admin.

---
+++ app/controllers/spree/admin/digitals_controller.rb
@@ -0,0 +1,21 @@
+module Spree
+  module Admin
+    class DigitalsController < ResourceController
+      belongs_to 'spree/product', find_by: :slug
+
+      private
+
+      def location_after_save
+        spree.admin_product_digitals_path(@product)
+      end
+
+      def permitted_resource_params
+        params.require(:digital).permit(permitted_digital_attributes)
+      end
+
+      def permitted_digital_attributes
+        %i[variant_id attachment attachment_file_name attachment_content_type]
+      end
+    end
+  end
+end
