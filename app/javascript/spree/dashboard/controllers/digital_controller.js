5bcaf74980249c61e9b3cf2e13e4caa3f27b9aa5
Matthew Kennedy <m.kennedy@me.com>
2021-11-13 22:05:59 +0000

    Move to stimulus

---
+++ app/javascript/spree/dashboard/controllers/digital_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "name" ]
+
+  greet() {
+    console.log(`Hello, ${this.name}!`)
+  }
+
+}
--- rollup.config.js
+++ rollup.config.js
@@ -15,7 +15,7 @@ export default [
   {
     input: "app/javascript/spree/dashboard/index.js",
     output: {
-      file: "app/assets/javascripts/spree_dashboard.js",
+      file: "app/assets/javascripts/spree-dashboard.js",
       format: "umd",
       name: "SpreeDashboard"
     },
@@ -29,7 +29,7 @@ export default [
   {
     input: "app/javascript/spree/dashboard/index.js",
     output: {
-      file: "app/assets/javascripts/spree_dashboard.esm.js",
+      file: "app/assets/javascripts/spree-dashboard.esm.js",
       format: "es"
     },
     plugins: [
