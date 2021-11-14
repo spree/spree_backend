d8f2c0165a6fd0b6e195278a49b84d979c480929
Matthew Kennedy <m.kennedy@me.com>
2021-11-14 15:04:01 +0000

    Stop adding spree/dashboard to package.json

--- lib/generators/spree/backend/install/install_generator.rb
+++ lib/generators/spree/backend/install/install_generator.rb
@@ -16,7 +16,6 @@ module Spree
           template 'app/javascript/spree-dashboard.js'
           template 'vendor/assets/javascripts/spree/backend/all.js'
           template 'vendor/assets/stylesheets/spree/backend/all.css'
-          run 'yarn add @spree/dashboard'
         end
       end
     end
