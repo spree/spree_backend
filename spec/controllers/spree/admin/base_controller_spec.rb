884a1df897f9df5a34d44dce52d35575404a70b9
Damian Legawiec <damian@sparksolutions.co>
2021-11-23 19:22:30 +0100

    Fixed specs broken by https://github.com/spree/spree/commit/9d335d35583ae0783fda542d46053f642c3b8dfe

--- spec/controllers/spree/admin/base_controller_spec.rb
+++ spec/controllers/spree/admin/base_controller_spec.rb
@@ -46,7 +46,7 @@ describe Spree::Admin::BaseController, type: :controller do
     end
     context 'when logged in' do
       before do
-        allow(controller).to receive_messages(try_spree_current_user: double('User', id: 1, last_incomplete_spree_order: nil))
+        allow(controller).to receive_messages(try_spree_current_user: double('User', id: 1, last_incomplete_spree_order: nil, persisted?: true))
       end
 
       it 'redirects forbidden path' do
