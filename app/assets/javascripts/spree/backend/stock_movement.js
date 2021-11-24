4fcd4bf78c56a8fb5499757c7b2fba2a4ecfb708
Aleksandar Petrushev <aleksandar.petrushev@sparksolutions.co>
2021-11-24 11:53:03 +0100

    Set proper stock_item_id when creating stock movement (#45)
    * Set proper stock_item_id when creating stock movement
    
    * Send StockItem id when creating a stock movement
    
    * Fix indendation in StockMovementsController

--- app/assets/javascripts/spree/backend/stock_movement.js
+++ app/assets/javascripts/spree/backend/stock_movement.js
@@ -4,16 +4,22 @@ $(function () {
   var jsonApiVariants = {}
   el.select2({
     placeholder: 'Find a stock item', // translate
+    minimumInputLength: 3,
+    quietMillis: 200,
     ajax: {
       url: Spree.routes.stock_items_api_v2,
-      data: function (term, page) {
+      data: function (params, page) {
         return {
           filter: {
-            variant_product_name_cont: term,
+            variant_product_name_cont: params.term,
             stock_location_id_eq: el.data('stock-location-id')
           },
+          include: 'variant',
+          image_transformation: {
+            size: '100x100'
+          },
           fields: {
-            'variant': 'name,sku,options_text'
+            'variant': 'name,sku,options_text,images'
           },
           per_page: 50,
           page: page
@@ -28,7 +34,11 @@ $(function () {
       },
       processResults: function (json) {
         var res = jsonApiVariants.map(function (stockItem) {
-          return formattedVariantList(stockItem.variant)
+          return {
+            id: stockItem.id,
+            text: stockItem.variant.name,
+            variant: stockItem.variant
+          }
         })
 
         return {
@@ -43,13 +53,28 @@ $(function () {
         }
       }
     },
-    formatResult: function (stockItem) {
-      return variantTemplate({
-        variant: stockItem.variant
-      })
+    templateResult: function (stockItem) {
+      if (stockItem.loading) {
+        return stockItem.text
+      }
+
+      let variant = stockItem.variant
+      if (variant['images'][0] !== undefined && variant['images'][0].transformed_url !== undefined) {
+        variant.image = variant.images[0].transformed_url
+      }
+      return $(variantTemplate({
+        variant: variant
+      }))
     },
-    formatSelection: function (stockItem) {
-      return Select2.util.escapeMarkup(stockItem.variant.name + '(' + stockItem.variant.options_text + ')')
+    templateSelection: function(stockItem) {
+      const variant = stockItem.variant
+      if (variant === undefined) {
+        return stockItem.text
+      } else if (!!variant.options_text && variant.options_text !== '') {
+        return variant.name + '(' + variant.options_text + ')'
+      } else {
+        return variant.name
+      }
     }
   })
 })
