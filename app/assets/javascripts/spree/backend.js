133a1148f6a07ac45c4c2d0f3e4e8852f8704d23
Matthew Kennedy <m.kennedy@me.com>
2021-11-24 21:26:58 +0000

    Clean up index js.

--- app/assets/javascripts/spree/backend.js
+++ app/assets/javascripts/spree/backend.js
@@ -8,8 +8,6 @@
 //= require select2-full
 //= require sweetalert2
 //= require tinymce
-//= require spree/backend/global/tinymce
-
 //= require underscore-min.js
 //= require jsonapi-serializer.min
 
