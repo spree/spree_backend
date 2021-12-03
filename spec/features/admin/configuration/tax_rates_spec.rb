92bdf5033f11b6adad344df117abf6691db7f70a
Damian Legawiec <damian@getvendo.com>
2021-12-03 22:39:25 +0100

    Fixed flaky spec

--- spec/features/admin/configuration/tax_rates_spec.rb
+++ spec/features/admin/configuration/tax_rates_spec.rb
@@ -13,8 +13,9 @@ describe 'Tax Rates', type: :feature do
     within find('#contentHeader') do
       click_link 'New Tax Rate'
     end
+    fill_in 'Name', with: 'My Tax'
     fill_in 'Rate', with: '0.05'
     click_button 'Create'
-    expect(page).to have_content('Tax Rate has been successfully created!')
+    expect(page).to have_content('Tax Rate "My Tax" has been successfully created!')
   end
 end
