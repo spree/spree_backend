b430e689bc1ae00c918e01216f320eb9573a5a87
Aleksandar Petrushev <aleksandar.petrushev@sparksolutions.co>
2021-12-15 15:37:58 +0100

    Use url helper for store switching

--- app/helpers/spree/admin/stores_helper.rb
+++ app/helpers/spree/admin/stores_helper.rb
@@ -22,7 +22,7 @@ module Spree
           icon = svg_icon name: 'circle.svg', width: '18', height: '18'
         end
 
-        link_to icon + store.unique_name, URI.join(url_for(host: store.formatted_url), '/admin').to_s,
+        link_to icon + store.unique_name, spree.admin_url(host: store.formatted_url),
                 class: "#{classes} text-dark p-3 dropdown-item", id: store.code, data: { turbo: false }
       end
     end
