cf7c84962a77d56dd876a631b4aa508bc59f2166
Matthew Kennedy <m.kennedy@me.com>
2022-01-03 14:35:47 +0000

    Improve store search.

--- app/javascript/spree/dashboard/controllers/search_controller.js
+++ app/javascript/spree/dashboard/controllers/search_controller.js
@@ -1,33 +1,37 @@
+/* eslint-disable no-undef */
+
 import { Controller } from "@hotwired/stimulus"
 import autoComplete from "@tarekraafat/autocomplete.js"
 import { get } from "../utilities/request_utility"
 
 export default class extends Controller {
   connect() {
-    const autoCompleteJS = new autoComplete(
+    const storeSearch = new autoComplete(
       {
-        placeHolder: "Search",
+        placeHolder: Spree.translations.search,
+        selector: "#storeSearch",
         data: {
           src: async (query) => {
             try {
+
               // Taxons
-              const taxons = await get(`/api/v2/platform/taxons?filter[name_cont]=${query}`)
+              const taxons = await get(`${Spree.routes.taxons_api_v2}?filter[name_cont]=${query}`)
               const taxons_response = await taxons.json
 
               // Products
-              const variants = await get(`/api/v2/platform/variants?filter[product_name_or_sku_cont]=${query}`)
+              const variants = await get(`${Spree.routes.variants_api_v2}?filter[product_name_or_sku_cont]=${query}`)
               const variants_response = await variants.json
 
               // Users
-              const users = await get(`/api/v2/platform/users?filter[email_cont]=${query}`)
+              const users = await get(`${Spree.routes.users_api_v2}?filter[email_cont]=${query}`)
               const users_response = await users.json
 
               // Orders
-              const orders = await get(`/api/v2/platform/orders?filter[number_or_email_cont]=${query}`)
+              const orders = await get(`${Spree.routes.orders_api_v2}?filter[number_or_email_cont]=${query}`)
               const orders_response = await orders.json
 
               // Pages
-              const pages = await get(`/api/v2/platform/cms_pages?filter[title_cont]=${query}`)
+              const pages = await get(`${Spree.routes.pages_api_v2}?filter[title_cont]=${query}`)
               const pages_response = await pages.json
 
               return formatResponse(taxons_response, variants_response, users_response, orders_response, pages_response)
@@ -103,7 +107,7 @@ export default class extends Controller {
       taxons.data.forEach(function (taxon) {
         response_array.push({
           "taxon": taxon.attributes.name,
-          "uri": `/admin/taxonomies/${taxon.relationships.taxonomy.data.id}/taxons/${taxon.id}/edit`,
+          "uri": `${Spree.adminPath}taxonomies/${taxon.relationships.taxonomy.data.id}/taxons/${taxon.id}/edit`,
           "pretty_name": taxon.attributes.pretty_name,
           "permalink": taxon.attributes.permalink,
 
@@ -115,7 +119,7 @@ export default class extends Controller {
       variants.data.forEach(function (variant) {
         response_array.push({
           "product": variant.attributes.name + " " + variant.attributes.sku,
-          "uri": `/admin/products/${variant.relationships.product.data.id}/edit`,
+          "uri": `/${Spree.adminPath()}products/${variant.relationships.product.data.id}/edit`,
           "sku": variant.attributes.sku,
           "name": variant.attributes.name,
 
@@ -127,7 +131,7 @@ export default class extends Controller {
       users.data.forEach(function (user) {
         response_array.push({
           "user": user.attributes.email,
-          "uri": `/admin/users/${user.id}/edit`,
+          "uri": `/${Spree.adminPath()}users/${user.id}/edit`,
 
           // Placeholders
           "product": "", "order": "", "taxon": "", "page": ""
@@ -138,7 +142,7 @@ export default class extends Controller {
         console.log(order)
         response_array.push({
           "order": order.attributes.number + " " + order.attributes.email,
-          "uri": `/admin/orders/${order.attributes.number}/edit`,
+          "uri": `/${Spree.adminPath()}orders/${order.attributes.number}/edit`,
           "email": order.attributes.email,
           "number": order.attributes.number,
           "state": order.attributes.state,
@@ -151,7 +155,7 @@ export default class extends Controller {
       pages.data.forEach(function (page) {
         response_array.push({
           "page": page.attributes.title,
-          "uri": `/admin/cms_pages/${page.id}/edit`,
+          "uri": `/${Spree.adminPath()}cms_pages/${page.id}/edit`,
 
           // Placeholders
           "order": "", "product": "", "user": "", "taxon": ""
@@ -162,8 +166,8 @@ export default class extends Controller {
     }
 
     // Redirect to URI on selection
-    autoCompleteJS.input.addEventListener("selection", function (event) {
-      const uri = event.detail.selection.value["uri"]
+    storeSearch.input.addEventListener("selection", function (event) {
+      const uri = event.detail.selection.value.uri
 
       Turbo.visit(uri)
     })
