0b2f1e83c3445165904c9c85ff4a4b2c094c7b26
Matthew Kennedy <m.kennedy@me.com>
2022-02-08 08:16:27 +0000

    Small updates.

--- app/controllers/concerns/spree/admin/metadata_concern.rb
+++ app/controllers/concerns/spree/admin/metadata_concern.rb
@@ -5,13 +5,21 @@ module Spree
 
       included do
         def assert_metadata(object)
+          # How can we make this usable for both types of metadata...
           return unless params[:public_metadata].present?
 
           params[:public_metadata][:key].each_with_index do |key, i|
             next unless key.present?
 
-            object.public_metadata[key.to_sym] = params[:public_metadata][:value][i]
+            object.public_metadata[format_key(key)] = params[:public_metadata][:value][i]
           end
+        end
+
+        private
+
+        def format_key(key)
+          # More formatting is required...
+          key.downcase.to_sym
         end
       end
     end
