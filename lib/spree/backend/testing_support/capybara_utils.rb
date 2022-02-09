a9de46fde7d354e5d0ca41cc58242f756de86f4b
Matthew Kennedy <m.kennedy@me.com>
2022-02-09 11:54:37 +0000

    Fix CC issues.

--- lib/spree/backend/testing_support/capybara_utils.rb
+++ lib/spree/backend/testing_support/capybara_utils.rb
@@ -31,13 +31,13 @@ module Spree
         end
 
         def wait_for_turbo(timeout = nil)
-          if has_css?('.turbo-progress-bar', visible: true, wait: (0.25).seconds)
+          if has_css?('.turbo-progress-bar', visible: true, wait: 0.25.seconds)
             has_no_css?('.turbo-progress-bar', wait: timeout.presence || 5.seconds)
           end
         end
 
         def wait_for_turbo_frame(selector = 'turbo-frame', timeout = nil)
-          if has_selector?("#{selector}[busy]", visible: true, wait: (0.25).seconds)
+          if has_selector?("#{selector}[busy]", visible: true, wait: 0.25.seconds)
             has_no_selector?("#{selector}[busy]", wait: timeout.presence || 5.seconds)
           end
         end
