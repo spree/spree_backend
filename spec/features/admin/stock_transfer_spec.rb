792d8fcf41a07434dccdb94404e1b4dee27920a0
Matthew Kennedy <m.kennedy@me.com>
2022-02-09 09:52:31 +0000

    Fix some tests in stock_loction.

--- spec/features/admin/stock_transfer_spec.rb
+++ spec/features/admin/stock_transfer_spec.rb
@@ -1,126 +1,130 @@
-# This whole spec needs ATT
+require 'spec_helper'
 
-# require 'spec_helper'
+describe 'Stock Transfers', type: :feature, js: true do
+  stub_authorization!
 
-# describe 'Stock Transfers', type: :feature, js: true do
-#   stub_authorization!
+  let!(:product) { create(:product_in_stock, name: 'Fluffy Toy Monkey', sku: 'A99400', description: 'lorem ipsum') }
+  let!(:variant) { create(:variant, product: product, sku: 'MIOD') }
 
-#   let!(:product) { create(:product, name: 'Fluffy Toy Monkey', sku: 'A99400', description: 'lorem ipsum') }
-#   let!(:variant) { create(:variant, product: product) }
+  it 'shows variants with options text' do
+    create(:stock_location_with_items, name: 'NY')
+    variant.set_option_value('Color', 'Green')
 
-#   it 'shows variants with options text' do
-#     create(:stock_location_with_items, name: 'NY')
-#     variant.set_option_value('Color', 'Green')
+    visit spree.admin_stock_transfers_path
+    click_on 'New Stock Transfer'
 
-#     visit spree.admin_stock_transfers_path
-#     click_on 'New Stock Transfer'
+    select2_open label: 'Variant'
+    select2_search variant.sku, from: 'Variant'
+    select2_select variant.name, from: 'Variant', match: :first
 
-#     select2_open label: 'Variant'
-#     select2_search variant.sku, from: 'Variant'
-#     select2_select variant.name, from: 'Variant', match: :first
+    content = "#{variant.name} (#{variant.options_text}) - #{variant.sku}"
+    expect(page).to have_content(content)
+  end
 
-#     content = "#{variant.name} (#{variant.options_text}) - #{variant.sku}"
-#     expect(page).to have_content(content)
-#   end
+  # TODO: FIX ME
+  xcontext 'ERROR: TODO -> Some variants are not available on Stock location' do
+    it 'transfer between 2 locations' do
+      create(:stock_location, name: 'SF') # destination_location
 
-#   it 'transfer between 2 locations' do
-#     create(:stock_location_with_items, name: 'NY') # source_location
-#     create(:stock_location, name: 'SF') # destination_location
+      variant = Spree::Variant.last
 
-#     visit spree.admin_stock_transfers_path
-#     click_on 'New Stock Transfer'
-#     fill_in 'reference', with: 'PO 666'
+      visit spree.admin_stock_transfers_path
 
-#     select2_open label: 'Variant'
-#     select2_search 'Fluffy Toy Monkey', from: 'Variant'
-#     select2_select 'Fluffy Toy Monkey', from: 'Variant', match: :first
+      click_on 'New Stock Transfer'
+      fill_in 'reference', with: 'PO 666'
 
-#     click_button 'Add'
-#     click_button 'Transfer Stock'
+      select2 variant.stock_locations.first.name, from: 'Source'
+      wait_for_turbo
+      select2 variant.sku, from: 'Variant', search: true
 
-#     expect(page).to have_content('Reference PO 666')
-#     expect(page).to have_content('NY')
-#     expect(page).to have_content('SF')
-#     expect(page).to have_content('Fluffy Toy Monkey')
+      click_button 'Add'
+      click_button 'Transfer Stock'
 
-#     transfer = Spree::StockTransfer.last
-#     expect(transfer.stock_movements.size).to eq 2
-#   end
+      wait_for_turbo
 
-#   it 'does not transfer if variant is not available on hand' do
-#     create(:stock_location_with_items, name: 'NY') # source_location
-#     create(:stock_location, name: 'SF') # destination_location
+      expect(page).to have_content('Reference PO 666')
+      expect(page).to have_content('NY')
+      expect(page).to have_content('SF')
+      expect(page).to have_content('Fluffy Toy Monkey')
 
-#     product = create(:product, stores: Spree::Store.all)
-#     Spree::StockLocation.first.stock_items.where(variant_id: product.master.id).first.adjust_count_on_hand(0)
+      transfer = Spree::StockTransfer.last
+      expect(transfer.stock_movements.size).to eq 2
+    end
+  end
 
-#     visit spree.admin_stock_transfers_path
-#     click_on 'New Stock Transfer'
+  it 'does not transfer if variant is not available on hand' do
+    create(:stock_location_with_items, name: 'NY') # source_location
+    create(:stock_location, name: 'SF') # destination_location
 
-#     fill_in 'reference', with: 'PO 666'
+    Spree::StockLocation.first.stock_items.where(variant_id: product.master.id).first.adjust_count_on_hand(0)
 
-#     select2_open label: 'Variant'
-#     select2_search product.master.name, from: 'Variant'
-#     select2_select product.master.name, from: 'Variant', match: :first
+    visit spree.admin_stock_transfers_path
+    click_on 'New Stock Transfer'
 
-#     click_button 'Add'
+    fill_in 'reference', with: 'PO 666'
 
-#     click_button 'Transfer Stock'
+    select2_open label: 'Variant'
+    select2_search product.master.name, from: 'Variant'
+    select2_select product.master.name, from: 'Variant', match: :first
 
-#     expect(page).to have_content('Some variants are not available')
-#   end
+    click_button 'Add'
 
-#   describe 'received stock transfer' do
-#     def it_is_received_stock_transfer(page)
-#       expect(page).to have_content('Reference PO 666')
-#       expect(page).not_to have_selector('#stock-location-source')
-#       expect(page).to have_selector('#stock-location-destination')
+    click_button 'Transfer Stock'
 
-#       transfer = Spree::StockTransfer.last
-#       expect(transfer.stock_movements.size).to eq 1
-#       expect(transfer.source_location).to be_nil
-#     end
+    expect(page).to have_content('Some variants are not available')
+  end
 
-#     it 'receive stock to a single location' do
-#       create(:stock_location_with_items, name: 'NY') # source_location
-#       create(:stock_location, name: 'SF') # destination_location
+  describe 'received stock transfer' do
+    def it_is_received_stock_transfer(page)
+      expect(page).to have_content('Reference PO 666')
+      expect(page).not_to have_selector('#stock-location-source')
+      expect(page).to have_selector('#stock-location-destination')
 
-#       variant = Spree::Variant.last
+      transfer = Spree::StockTransfer.last
+      expect(transfer.stock_movements.size).to eq 1
+      expect(transfer.source_location).to be_nil
+    end
 
-#       visit spree.new_admin_stock_transfer_path
+    it 'receive stock to a single location' do
+      create(:stock_location_with_items, name: 'NY') # source_location
+      create(:stock_location, name: 'SF') # destination_location
 
-#       fill_in 'reference', with: 'PO 666'
-#       check 'transfer_receive_stock'
-#       select2 'NY', from: 'Destination'
+      visit spree.new_admin_stock_transfer_path
 
-#       select2_open label: 'Variant'
-#       select2_search variant.name, from: 'Variant'
-#       select2_select variant.name, from: 'Variant', match: :first
+      fill_in 'reference', with: 'PO 666'
+      check 'transfer_receive_stock'
+      select2 'NY', from: 'Destination'
 
-#       click_button 'Add'
-#       click_button 'Transfer Stock'
+      select2_open label: 'Variant'
+      select2_search variant.name, from: 'Variant'
+      select2_select variant.name, from: 'Variant', match: :first
 
-#       it_is_received_stock_transfer page
-#     end
+      click_button 'Add'
+      click_button 'Transfer Stock'
 
-#     it 'forced to only receive there is only one location' do
-#       create(:stock_location_with_items, name: 'NY') # source_location
-#       variant = Spree::Variant.last
+      it_is_received_stock_transfer page
+    end
 
-#       visit spree.new_admin_stock_transfer_path
+    # TODO: FIX ME
+    xcontext 'ERROR: TODO -> Some variants are not available on Stock location' do
+      it 'forced to only receive there is only one location' do
+        create(:stock_location_with_items, name: 'NY') # source_location
 
-#       fill_in 'reference', with: 'PO 666'
+        visit spree.new_admin_stock_transfer_path
 
-#       select2 'NY', from: 'Destination'
+        fill_in 'reference', with: 'PO 666'
 
-#       select2_open label: 'Variant'
-#       select2_search variant.name, from: 'Variant'
-#       select2_select variant.name, from: 'Variant', match: :first
+        select2 'NY', from: 'Destination'
 
-#       click_button 'Add'
-#       click_button 'Transfer Stock'
+        select2_open label: 'Variant'
+        select2_search variant.name, from: 'Variant'
+        select2_select variant.name, from: 'Variant', match: :first
 
-#       it_is_received_stock_transfer page
-#     end
-#   end
-# end
+        click_button 'Add'
+        click_button 'Transfer Stock'
+
+        it_is_received_stock_transfer page
+      end
+    end
+  end
+end
