72565b629ce5413bfba6a0bb1711d693b3f85820
Matthew Kennedy <m.kennedy@me.com>
2022-03-04 22:49:02 +0000

    Use hotwire/turbo

--- app/javascript/spree/dashboard/index.js
+++ app/javascript/spree/dashboard/index.js
@@ -24,7 +24,7 @@ application.debug = false
 // Exports To Window //
 ///////////////////////
 if (window instanceof Window) {
-  if (!window.Turbo) { require("@hotwired/turbo-rails") }
+  if (!window.Turbo) { require("@hotwired/turbo") }
   if (!window.Stimulus) { window.Stimulus = application }
   if (!window.RequestUtility) { window.RequestUtility = RequestUtility }
   if (!window.flatpickr) { window.flatpickr = Flatpickr }
