e32d0b12a1c691911c9f4f8fbc8cd68b66ab59b9
Matthew Kennedy <m.kennedy@me.com>
2022-03-06 10:21:35 +0000

    Clean up more.

--- app/javascript/spree/dashboard/index.js
+++ app/javascript/spree/dashboard/index.js
@@ -10,18 +10,11 @@ import jQuery from "jquery"
 import Bootstrap from "bootstrap"
 
 
-////////////////////
-// Stimulus Setup //
-////////////////////
-const application = Application.start()
-application.debug = false
-
-
 ///////////////////////
 // Exports To Window //
 ///////////////////////
 if (!window.Turbo)          { require("@hotwired/turbo-rails") }
-if (!window.Stimulus)       { window.Stimulus = application }
+if (!window.Stimulus)       { window.Stimulus = Application.start() }
 if (!window.RequestUtility) { window.RequestUtility = RequestUtility }
 if (!window.flatpickr)      { window.flatpickr = Flatpickr }
 if (!window.jQuery)         { window.$ = window.jQuery = jQuery }
@@ -33,26 +26,26 @@ if (!window.bootstrap)      { window.bootstrap = Bootstrap }
 import "./utilities/bootstrap"
 
 
-//////////////////////////
-// Stimulus Controllers //
-//////////////////////////
+//////////////
+// Stimulus //
+//////////////
 import UploadButtonController from "./controllers/upload_button_controller"
-application.register("upload-button", UploadButtonController)
+Stimulus.register("upload-button", UploadButtonController)
 
 import SpreeController from "./controllers/spree_controller"
-application.register("spree", SpreeController)
+Stimulus.register("spree", SpreeController)
 
 import SortableTreeController from "./controllers/sortable_tree_controller"
-application.register("sortable-tree", SortableTreeController)
+Stimulus.register("sortable-tree", SortableTreeController)
 
 import WebhooksSubscriberEventsController from "./controllers/webhooks_subscriber_events_controller"
-application.register("webhooks-subscriber-events", WebhooksSubscriberEventsController)
+Stimulus.register("webhooks-subscriber-events", WebhooksSubscriberEventsController)
 
 import PasswordToggleController from "./controllers/password_toggle_controller"
-application.register("password-toggle", PasswordToggleController)
+Stimulus.register("password-toggle", PasswordToggleController)
 
 import ClipboardController from "./controllers/clipboard_controller"
-application.register("clipboard", ClipboardController)
+Stimulus.register("clipboard", ClipboardController)
 
 import ProductEditController from "./controllers/product_edit_controller"
-application.register("product-edit", ProductEditController)
+Stimulus.register("product-edit", ProductEditController)
