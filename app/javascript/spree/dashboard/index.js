a682bc05455b02b9c6cd7be452dacd1ac12fe488
Matthew Kennedy <m.kennedy@me.com>
2022-03-07 09:01:23 +0000

    Add Stimulus to window.

--- app/javascript/spree/dashboard/index.js
+++ app/javascript/spree/dashboard/index.js
@@ -37,6 +37,7 @@ import "./utilities/bootstrap"
 //////////////
 const application = Application.start()
 application.debug = false
+window.Stimulus = application
 
 import UploadButtonController from "./controllers/upload_button_controller"
 application.register("upload-button", UploadButtonController)
