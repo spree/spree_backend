223d876e6c7339afc3f2ddceb6e1ecb78ffb53e8
Matthew Kennedy <m.kennedy@me.com>
2022-03-13 18:10:58 +0000

    Improve

--- app/controllers/spree/admin/base_controller.rb
+++ app/controllers/spree/admin/base_controller.rb
@@ -61,7 +61,7 @@ module Spree
         Spree.t(event_sym, resource: resource_desc)
       end
 
-      def render_flash(kind, message)
+      def render_flash(message:, kind:)
         respond_to do |format|
           format.turbo_stream do
             render turbo_stream: turbo_stream.append('FlashAlertsContainer', partial: 'spree/admin/shared/flash_notice', locals: { message: message, kind: kind })
