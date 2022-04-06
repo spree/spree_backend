88a02ea26789e8ddd01c05f323e64ab577494dd9
Matthew Kennedy <m.kennedy@me.com>
2022-04-06 22:21:21 +0100

    Organize stimulus index

--- app/javascript/spree/dashboard/controllers/index.js
+++ app/javascript/spree/dashboard/controllers/index.js
@@ -1,25 +1,21 @@
 /* eslint-disable no-undef */
 
 import { Application } from "@hotwired/stimulus"
-if (!window.Stimulus) { window.Stimulus = Application.start() }
 
 import UploadButtonController from "./upload_button_controller"
-Stimulus.register("upload-button", UploadButtonController)
-
 import SpreeController from "./spree_controller"
-Stimulus.register("spree", SpreeController)
-
 import SortableTreeController from "./sortable_tree_controller"
-Stimulus.register("sortable-tree", SortableTreeController)
-
 import WebhooksSubscriberEventsController from "./webhooks_subscriber_events_controller"
-Stimulus.register("webhooks-subscriber-events", WebhooksSubscriberEventsController)
-
 import PasswordToggleController from "./password_toggle_controller"
-Stimulus.register("password-toggle", PasswordToggleController)
-
 import ClipboardController from "./clipboard_controller"
-Stimulus.register("clipboard", ClipboardController)
-
 import ProductEditController from "./product_edit_controller"
+
+if (!window.Stimulus) { window.Stimulus = Application.start() }
+
+Stimulus.register("upload-button", UploadButtonController)
+Stimulus.register("spree", SpreeController)
+Stimulus.register("sortable-tree", SortableTreeController)
+Stimulus.register("webhooks-subscriber-events", WebhooksSubscriberEventsController)
+Stimulus.register("password-toggle", PasswordToggleController)
+Stimulus.register("clipboard", ClipboardController)
 Stimulus.register("product-edit", ProductEditController)
