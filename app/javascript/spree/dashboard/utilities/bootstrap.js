6316b58ab76abf6dcf2cc33b3f5360e796d8d8f2
Matthew Kennedy <m.kennedy@me.com>
2022-04-20 20:25:50 +0100

    Fix CC issues.

--- app/javascript/spree/dashboard/utilities/bootstrap.js
+++ app/javascript/spree/dashboard/utilities/bootstrap.js
@@ -1,13 +1,15 @@
-import 'bootstrap'
-import 'popper.js'
+/* eslint-disable no-undef */
+
+import "bootstrap"
+import "popper.js"
 
 document.addEventListener("spree:load", function() {
-  $('.with-tip').each(function() {
+  $(".with-tip").each(function() {
     $(this).tooltip()
   })
 
-  $('.with-tip').on('show.bs.tooltip', function(event) {
-    if (('ontouchstart' in window)) {
+  $(".with-tip").on("show.bs.tooltip", function(event) {
+    if (("ontouchstart" in window)) {
       event.preventDefault()
     }
   })
