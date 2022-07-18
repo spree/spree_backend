7faf241803c2b03b65d41799dea9f0312025f611
Rafal Kosla <rafalkosla101@gmail.com>
2022-07-18 08:24:49 +0200

    Add YAML Symbol to assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,5 @@ Rails.application.config.assets.precompile << 'spree_backend_manifest.js'
 
 Rails.application.config.assets.configure do |env|
   env.export_concurrent = false
+  Rails.application.config.active_record.yaml_column_permitted_classes = [Symbol]
 end
