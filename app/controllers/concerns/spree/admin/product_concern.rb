100715d1ecb2d7f6d7492a72b60d2032d87e062f
aryt <adrianryt3@gmail.com>
2022-07-22 15:04:37 +0200

    Join translations table for product scope

--- app/controllers/concerns/spree/admin/product_concern.rb
+++ app/controllers/concerns/spree/admin/product_concern.rb
@@ -5,6 +5,7 @@ module Spree
 
       def product_scope
         current_store.products.accessible_by(current_ability, :index)
+                     .joins(:translations).where(spree_product_translations: { locale: I18n.locale })
       end
     end
   end
