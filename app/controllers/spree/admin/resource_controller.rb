d03bc4fd3e76bed6699e543ed6fd994185cd23aa
Matthew Kennedy <m.kennedy@me.com>
2022-07-25 10:10:26 +0100

    Separate turbo stream activation methods

--- app/controllers/spree/admin/resource_controller.rb
+++ app/controllers/spree/admin/resource_controller.rb
@@ -30,6 +30,7 @@ class Spree::Admin::ResourceController < Spree::Admin::BaseController
       set_current_store
       invoke_callbacks(:update, :after)
       respond_with(@object) do |format|
+        format.turbo_stream if update_turbo_stream_enabled?
         format.html do
           flash[:success] = flash_message_for(@object, :successfully_updated)
           redirect_to location_after_save unless request.xhr?
@@ -53,8 +54,8 @@ class Spree::Admin::ResourceController < Spree::Admin::BaseController
       invoke_callbacks(:create, :after)
       flash[:success] = flash_message_for(@object, :successfully_created)
       respond_with(@object) do |format|
+        format.turbo_stream if create_turbo_stream_enabled?
         format.html { redirect_to location_after_save }
-        format.turbo_stream if turbo_enabled?
         format.js   { render layout: false }
       end
     else
@@ -281,7 +282,11 @@ class Spree::Admin::ResourceController < Spree::Admin::BaseController
     [:new, :create]
   end
 
-  def turbo_enabled?
+  def create_turbo_stream_enabled?
+    false
+  end
+
+  def update_turbo_stream_enabled?
     false
   end
 end
