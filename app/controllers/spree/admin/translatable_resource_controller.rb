3f571e21caeb708372942d957b525bf8fec9fe5e
Rafal Kosla <rafalkosla101@gmail.com>
2022-12-19 14:02:46 +0100

    Fix translation table not showing and add translations to taxonomies

--- app/controllers/spree/admin/translatable_resource_controller.rb
+++ app/controllers/spree/admin/translatable_resource_controller.rb
@@ -13,17 +13,12 @@ module Spree
         @object.save
         flash[:success] = Spree.t('notice_messages.translations_saved')
         # TODO there should be a better way to redirect
-        @object.instance_of?(Spree::Product) ? redirect_to_product_edit_page : redirect_to_taxon_edit_page
-      end
-
-      def redirect_to_product_edit_page
-        redirect_to spree.edit_admin_product_url(@object)
+        @object.instance_of?(Spree::Taxon) ? redirect_to_taxon_edit_page : redirect_to(edit_polymorphic_path([:admin, @object]))
       end
 
       def redirect_to_taxon_edit_page
         redirect_to spree.edit_admin_taxonomy_taxon_path(@object.taxonomy, @object.id)
       end
-
     end
   end
 end
