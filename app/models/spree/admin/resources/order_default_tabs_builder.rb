4002e57463ad0cff6a2abba24081b29cb497b396
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-09-24 07:58:39 +0200

    Lay groundwork for order tabs builder

---
+++ app/models/spree/admin/resources/order_default_tabs_builder.rb
@@ -0,0 +1,11 @@
+module Spree
+  module Admin
+    module Resources
+      class OrderDefaultTabsBuilder
+        def build
+          
+        end
+      end
+    end
+  end
+end
---
+++ spec/models/spree/admin/resources/order_default_tabs_builder_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+module Spree
+  module Admin
+    describe Resources::OrderDefaultTabsBuilder, type: :model do
+      let(:builder) { described_class.new }
+      let(:default_tabs) do
+        %w(cart
+          channel
+          customer
+          shipments
+          adjustments
+          payments
+          return_authorizations
+          customer_returns
+          state_changes)
+      end
+
+      describe '#build' do
+        subject { builder.build }
+
+        it 'builds default tabs' do
+          # this means that tab items will need to respond to 'text' message,
+          # just as section items respond to 'key' message
+          expect(subject.items.map(&:text)).to match(default_tabs)
+        end
+      end
+    end
+  end
+end
