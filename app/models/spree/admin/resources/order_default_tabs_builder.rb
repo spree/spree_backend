55abda62ad9090767020947375374d68683d477d
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-09-26 09:43:09 +0200

    Add methods for building particular tabs

--- app/models/spree/admin/resources/order_default_tabs_builder.rb
+++ app/models/spree/admin/resources/order_default_tabs_builder.rb
@@ -2,9 +2,50 @@ module Spree
   module Admin
     module Resources
       class OrderDefaultTabsBuilder
+        include Spree::Core::Engine.routes.url_helpers
+
         def build
-          
+          root = Root.new
+          add_cart_tab(root)
+          add_channel_tab(root)
+          add_customer_tab(root)
+          add_shipments_tab(root)
+          add_adjustments_tab(root)
+          add_payments_tab(root)
+          add_return_authorizations_tab(root)
+          add_customer_returns_tab(root)
+          add_state_changes_tab(root)
         end
+
+        private
+
+        def add_cart_tab(root)
+        end
+
+        def add_channel_tab(root)
+        end
+
+        def add_customer_tab(root)
+        end
+
+        def add_shipments_tab(root)
+        end
+
+        def add_adjustments_tab(root)
+        end
+
+        def add_payments_tab(root)
+        end
+
+        def add_return_authorizations_tab(root)
+        end
+
+        def add_customer_returns_tab(root)
+        end
+
+        def add_state_changes_tab(root)
+        end
+
       end
     end
   end
