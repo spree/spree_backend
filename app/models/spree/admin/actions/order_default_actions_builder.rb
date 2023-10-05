e8001d3d14e9193d98b6220bea36bac84e51edcf
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-10-05 18:45:51 +0200

    Order#edit - use prebuilt action for resend

--- app/models/spree/admin/actions/order_default_actions_builder.rb
+++ app/models/spree/admin/actions/order_default_actions_builder.rb
@@ -6,10 +6,30 @@ module Spree
 
         def build
           root = Root.new
+          add_resend_action(root)
           root
         end
 
         private
+
+        def add_resend_action(root)
+          action =
+            ActionBuilder.new(resend_config).
+            with_resend_availability_check.
+            build
+
+          root.add(action)
+        end
+
+        def resend_config
+          {
+            icon_name: 'envelope.svg',
+            name: :resend,
+            url: ->(resource) { resend_admin_order_path(resource) },
+            classes: 'btn-secondary',
+            method: :post
+          }
+        end
       end
     end
   end
