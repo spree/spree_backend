2a627311ea2b827340188b3aa4d38eca5d0ee349
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-10-05 10:48:33 +0200

    Add module for availability checking

---
+++ app/models/spree/admin/actions/availability_builder_methods.rb
@@ -0,0 +1,32 @@
+module Spree
+  module Admin
+    module Actions
+      module AvailabilityBuilderMethods
+        def with_create_availability_check(klass)
+          @availability_check = ->(ability, _resource) { ability.can?(:create, klass) }
+          self
+        end
+
+        # def with_availability_check(availability_check)
+        #   @availability_check = availability_check
+        #   self
+        # end
+
+        # def with_update_availability_check
+        #   @availability_check = ->(ability, resource) { ability.can?(:update, resource) }
+        #   self
+        # end
+
+        # def with_index_availability_check(klass)
+        #   @availability_check = ->(ability, _resource) { ability.can?(:index, klass) }
+        #   self
+        # end
+
+        # def with_admin_availability_check(klass)
+        #   @availability_check = ->(ability, _resource) { ability.can?(:admin, klass) }
+        #   self
+        # end
+      end
+    end
+  end
+end
