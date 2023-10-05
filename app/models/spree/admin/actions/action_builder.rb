b7386ee6adaeefbd9df13e2dd326483b9a7dae06
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-10-05 10:48:08 +0200

    Add builder for admin actions

---
+++ app/models/spree/admin/actions/action_builder.rb
@@ -0,0 +1,65 @@
+module Spree
+  module Admin
+    module Actions
+      class ActionBuilder
+        include ConditionalChecker
+        include AvailabilityBuilderMethods
+
+        def initialize(config)
+          @icon_name = config[:icon_name]
+          @name = config[:name]
+          @url = config[:url]
+          @classes = config[:classes]
+          @availability_check = nil
+        end
+
+        def build
+          Tab.new(build_config)
+        end
+
+        private
+
+        def build_config
+          {
+            icon_name: @icon_name,
+            name: @name,
+            url: @url,
+            classes: @classes,
+            availability_check: @availability_check,
+            text: text
+          }
+        end
+
+        def text
+          ::Spree.t(@name)
+        end
+
+        # def available?(current_ability, resource)
+        #   return true unless @availability_check.present?
+
+        #   @availability_check.call(current_ability, resource)
+        # end
+
+        # def url(resource = nil)
+        #   @url.is_a?(Proc) ? @url.call(resource) : @url
+        # end
+
+        # def active?(current_tab)
+        #   @active_check.call(current_tab, partial_name)
+        # end
+
+        # def complete?(resource)
+        #   return true unless @completed_check.present?
+
+        #   @completed_check.call(resource)
+        # end
+
+        # def text
+        #   return true unless @translate.present?
+
+        #   @translate.call(name)
+        # end
+      end
+    end
+  end
+end
