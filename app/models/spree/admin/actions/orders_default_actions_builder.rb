be55d8b335980ab2e82adbde648522d2329758cc
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-10-05 17:26:51 +0200

    Orders#index - use prebuilt action object

--- app/models/spree/admin/actions/orders_default_actions_builder.rb
+++ app/models/spree/admin/actions/orders_default_actions_builder.rb
@@ -6,10 +6,29 @@ module Spree
 
         def build
           root = Root.new
+          add_new_order_action(root)
           root
         end
 
         private
+
+        def add_new_order_action(root)
+          action =
+            ActionBuilder.new(new_order_config).
+            with_create_availability_check(::Spree::Order).
+            build
+
+          root.add(action)
+        end
+
+        def new_order_config
+          {
+            icon_name: 'add.svg',
+            name: :new_order,
+            url: new_admin_order_path,
+            classes: 'btn-success'
+          }
+        end
       end
     end
   end
