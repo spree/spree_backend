b1a78026c0ee96c2daf484d37f0681cc55b88feb
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-10-10 19:56:11 +0200

    Allow multiple availability checks

--- app/models/spree/admin/actions/availability_builder_methods.rb
+++ app/models/spree/admin/actions/availability_builder_methods.rb
@@ -3,22 +3,27 @@ module Spree
     module Actions
       module AvailabilityBuilderMethods
         def with_create_availability_check(klass)
-          @availability_check = ->(ability, _resource) { ability.can?(:create, klass) }
+          @availability_checks << ->(ability, _resource) { ability.can?(:create, klass) }
           self
         end
 
         def with_resend_availability_check
-          @availability_check = ->(ability, resource) { ability.can?(:resend, resource) }
+          @availability_checks << ->(ability, resource) { ability.can?(:resend, resource) }
           self
         end
 
         def with_availability_check(availability_check)
-          @availability_check = availability_check
+          @availability_checks << availability_check
           self
         end
 
         def with_state_change_check(event)
-          @availability_check = ->(_ability, resource) { resource.send("can_#{event}?") }
+          @availability_checks << ->(_ability, resource) { resource.send("can_#{event}?") }
+          self
+        end
+
+        def with_fire_availability_check
+          @availability_checks << ->(ability, resource) { ability.can?(:fire, resource) }
           self
         end
 
