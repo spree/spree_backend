c958593f6e1c799e68a36d4b834ab8b5c454f088
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-10-12 08:01:45 +0200

    Cleanup unused code

--- app/models/spree/admin/actions/action.rb
+++ app/models/spree/admin/actions/action.rb
@@ -29,22 +29,6 @@ module Spree
         def url(resource = nil)
           @url.is_a?(Proc) ? @url.call(resource) : @url
         end
-
-        # def active?(current_tab)
-        #   @active_check.call(current_tab, partial_name)
-        # end
-
-        # def complete?(resource)
-        #   return true unless @completed_check.present?
-
-        #   @completed_check.call(resource)
-        # end
-
-        # def text
-        #   return true unless @translate.present?
-
-        #   @translate.call(name)
-        # end
       end
     end
   end
