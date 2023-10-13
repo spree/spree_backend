eaacccaadf48df87255e97778d2e72071688d15b
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-10-13 09:56:03 +0200

    Remove unused functions

--- app/models/spree/admin/actions/availability_builder_methods.rb
+++ app/models/spree/admin/actions/availability_builder_methods.rb
@@ -26,21 +26,6 @@ module Spree
           @availability_checks << ->(ability, resource) { ability.can?(:fire, resource) }
           self
         end
-
-        # def with_update_availability_check
-        #   @availability_check = ->(ability, resource) { ability.can?(:update, resource) }
-        #   self
-        # end
-
-        # def with_index_availability_check(klass)
-        #   @availability_check = ->(ability, _resource) { ability.can?(:index, klass) }
-        #   self
-        # end
-
-        # def with_admin_availability_check(klass)
-        #   @availability_check = ->(ability, _resource) { ability.can?(:admin, klass) }
-        #   self
-        # end
       end
     end
   end
