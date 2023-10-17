c47e0ec8d8862c9720d4d988283d725b8076a3b4
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-10-17 15:17:54 +0200

    Add missing test

--- spec/models/spree/admin/main_menu/root_spec.rb
+++ spec/models/spree/admin/main_menu/root_spec.rb
@@ -5,6 +5,25 @@ module Spree
     describe MainMenu::Root, type: :model do
       let(:root) { described_class.new }
 
+      describe '#add' do
+        let(:item) { double(key: 'test') }
+
+        context "when there's no item with a particular key" do
+          it 'appends an item' do
+            root.add(item)
+            expect(root.items).to include(item)
+          end
+        end
+
+        context 'when there is an item with a particular key' do
+          before { root.add(item) }
+
+          it 'raises an error' do
+            expect { root.add(item) }.to raise_error(KeyError)
+          end
+        end
+      end
+
       describe '#key' do
         subject { root.key }
 
