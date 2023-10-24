55363e12e6d173dccfdd3dfb0c4a3cd8fffa29ce
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-10-24 13:23:48 +0200

    Refactor Root class

--- app/models/spree/admin/actions/root.rb
+++ app/models/spree/admin/actions/root.rb
@@ -2,22 +2,12 @@ module Spree
   module Admin
     module Actions
       class Root
+        include ::Spree::Admin::ItemManager
+
         attr_reader :items
 
         def initialize
           @items = []
-        end
-
-        def add(item)
-          raise KeyError, "Item with key #{item.key} already exists" if index_for_key(item.key)
-
-          @items << item
-        end
-
-        private
-
-        def index_for_key(key)
-          @items.index { |e| e.key == key }
         end
       end
     end
