9bd79c7a844738a7de79ed6e160a7014cd55df9a
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-10-26 08:27:25 +0200

    Store_credits#index - use prebuilt action

---
+++ spec/models/spree/admin/actions/store_credits_default_actions_builder_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+module Spree
+  module Admin
+    describe Actions::StoreCreditsDefaultActionsBuilder, type: :model do
+      let(:builder) { described_class.new }
+      let(:default_actions) do
+        [:add_store_credit]
+      end
+
+      describe '#build' do
+        subject { builder.build }
+
+        it 'builds default tabs' do
+          expect(subject.items.map(&:key)).to match(default_actions)
+        end
+      end
+    end
+  end
+end
