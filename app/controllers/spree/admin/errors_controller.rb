8b233b94b38853d63d2d78a23f3cafe89f94cf12
Mateusz Porębski <mateusz.porebski99@gmail.com>
2024-01-09 09:32:23 +0100

    update ErrorsController to authorize access to Spree::AdminPanel

--- app/controllers/spree/admin/errors_controller.rb
+++ app/controllers/spree/admin/errors_controller.rb
@@ -1,10 +1,16 @@
 module Spree
   module Admin
     class ErrorsController < BaseController
+
       skip_before_action :authorize_admin
 
       def forbidden
+        authorize! :read, ::Spree::AdminPanel
         render status: 403
+      end
+
+      rescue_from CanCan::AccessDenied do |_exception|
+        throw _exception
       end
     end
   end
