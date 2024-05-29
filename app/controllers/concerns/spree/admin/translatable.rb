d14a8130233e2ad99acfed4542ecb0bdab340511
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2024-05-29 17:57:47 +0200

    Fix updating translations (#319)

--- app/controllers/concerns/spree/admin/translatable.rb
+++ app/controllers/concerns/spree/admin/translatable.rb
@@ -20,12 +20,25 @@ module Spree
         locales_to_update = current_store_locales & params_locales
 
         locales_to_update.each do |locale|
-          translation = @object.translations.find_or_initialize_by(locale: locale)
-          translation_params.each do |attribute, translations|
-            translation.public_send("#{attribute}=", translations[locale])
+          if update_translation_value?(locale)
+            translation = @object.translations.find_or_initialize_by(locale: locale)
+            translation_params.each do |attribute, translations|
+              translation.public_send("#{attribute}=", translations[locale])
+            end
+            translation.save!
+          else
+            Mobility.with_locale(locale) do
+              translation_params.each do |attribute, translations|
+                @object.public_send("#{attribute}=", translations[locale])
+              end
+              @object.save!
+            end
           end
-          translation.save!
         end
+      end
+
+      def update_translation_value?(locale)
+        Spree.always_use_translations? || I18n.default_locale.to_s != locale
       end
     end
   end
